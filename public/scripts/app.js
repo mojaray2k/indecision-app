d56e068efb398c57683dd23c7e5184c675adc92c
mojaray2k <amen.ra@imaginationeverywhere.info>
2018-02-21 01:58:48 -0500

    adding babel manually

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,13 @@
+'use strict';
+
 console.log('app.js is running');
 
 // JSX - Javascript XML
 var template = React.createElement(
-  "h1",
-  { id: "someid" },
-  "This is JSX from app.js"
+  'p',
+  null,
+  'This is JSX from app.js'
 );
-//var template = <p>This is JSX from app.js</p>;
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template,  appRoot);
+ReactDOM.render(template, appRoot);
