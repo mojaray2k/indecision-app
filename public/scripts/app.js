2526cf26fb0ec9b112e8e86f8b803e140dbc0fd0
mojaray2k <amen.ra@imaginationeverywhere.info>
2018-02-21 04:04:22 -0500

    nesting multiple && operators

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -61,7 +61,7 @@ var templateTwo = React.createElement(
     null,
     user.name ? user.name : 'Anonymous'
   ),
-  user.age >= 18 && React.createElement(
+  user.age && user.age >= 18 && React.createElement(
     'p',
     null,
     'Age: ',
